8e8717fd00d49fc98b68bbef39c6932d46912046
GabrielKitanovich <gabriel.kitanovich@gmail.com>
2025-02-14 17:15:20 -0300

    Add initial project structure and configuration for order service

---
+++ order-service/src/main/java/com/gabriel/microservices/order_service/model/Order.java
@@ -0,0 +1,23 @@
+package com.gabriel.microservices.order_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "T_orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
